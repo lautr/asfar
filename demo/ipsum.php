7826fa4b66090e4ffe06c9b695b5e19dbf0dad49
Jobhannes Lauter <hannes@lautr.com>
2011-12-06 16:50:32 +0100

    fixing some bugs, updaing demo and adding some things to the readme

--- demo/ipsum.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'includes/_core.php';
-core::start(__DIR__ . '/' . 'includes/_layout.html','Complete Request at ' . getmicrotime(2));
-?>
-<h2>Partial Request at <?php print getmicrotime(2); ?></h2>
-<p>Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
-<p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum</p>
--- demo/lorem.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/_core.php';
-core::start(__DIR__ . '/' . 'includes/_layout.html','Complete Request at ' . getmicrotime(2));
-?>
-<h2>Partial Request at <?php print getmicrotime(2); ?></h2>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
