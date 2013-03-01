d8d26421219ae224d47510f0f9b7468d784b0e96
lautr <hannes@lautr.com>
2013-03-01 19:30:47 +0100

    unstable update for version 2

--- source/asfar.jquery.js
+++ source/asfar.jquery.js
@@ -1,153 +1,197 @@
 /**
- * v1.1
+ * v1.2
  */
-(function ($) {
-	"use strict";
-	
-	function html5AjaxHistoryC(target, selector) {
-		var hash = location.hash,
-			target = target,
-			selector = selector,
-			pagecount = 0,
-			before,
-			insert,
-			success,
-			error,
-			after;
-		
-		if (typeof history.pushState === 'undefined') {
-			var html5 = false,
-				html5AjaxHistoryR;
-			
-			html5AjaxHistoryR = this;
-			
-			setInterval(function () {
-				if (location.hash !== hash) {
-					html5AjaxHistoryR.call(location.hash.substring(2));
-				}
-			}, 100);
-		} else {
-			var html5 = true,
-				html5AjaxHistoryR;
-			
-			html5AjaxHistoryR = this;
-			
-			window.onpopstate = function (event) {
-				if (event.state !== null) {
-					if ("ajax" === event.state.type) {
-						html5AjaxHistoryR.call(document.location.pathname, target, true);
-					}
-				}
-			};
-		}
-		
-		this.before = function (urlFragment, target) {};
-		this.success = function (urlFragment, target) {};
-		this.after = function (urlFragment, target) {};
-		this.error = function (urlFragment, target) {};
-		this.insert = function (urlFragment, target, data) {
-			$(target).html(data);
-		};
-				
-		this.call = function (urlFragment, target, history) {
-			target = typeof (target) !== 'undefined' ? target : '#content';
-			history = typeof (history) !== 'undefined' ? history : false;
-			
-			var thisR = this;
-			
-			thisR.before(urlFragment,target);
-			
-			$.ajax({
-				cache: false,
-				async: true,
-				type: 'GET',
-				url: urlFragment,
-				success: function(data) {
-					if (0 !== $(data).find(target).parent().size()) {
-						data = $(data).find(target).html();
-					}
-						
-					thisR.insert(urlFragment,target,data);
-					
-					if(0 !== $(target).find(selector).size()){
-						$(target).find(selector).click(function(){
-							event.preventDefault();
-							thisR.call($(this).attr('href'),target);
-						});
-					}
-				
-					
-					thisR.success(urlFragment,target);
-					
-					if (typeof callback == 'function') {
-						callback(urlFragment);
-					}
-				}
-			});
-			
-			thisR.after(urlFragment,target);
-			
-			if (true !== history) {
-				this.pushState(urlFragment);
-			}
-		
-		};
+;(function($, doc, win) {
+    "use strict";
 
-		this.pushState  = function (urlFragment)
-		{
-			if (true === html5) {
-				history.pushState({type: 'ajax'}, "title " + this.pagecount,urlFragment);
-				pagecount++;
-			}else{
-				location.hash = '#!' + urlFragment;
-				hash = location.hash;
-			}
-		}
-		
-		// init, set current page as first element
-		
-		this.pushState(document.location.pathname);
-	}
-	
-	$.fn.asfar = function () {
-		// place this in your header to optimize performance
-		if ('#!' === location.hash.substring(0,2)) {
+    var name = 'js-asfar';
+
+    function Asfar(el, opts, target) {
+        this.$el  = $(el);
+        this.pagecount = 0;
+        this.currentHash = null;
+        this.historyBackward = [];
+
+
+        this.default = {
+            'selector':        'a[href*="' + document.location.host + '"], a[href^="/"], a:not([href^="http://"])',
+            'target':           target,
+            'sheBangFallback': true,
+            'sheBangInterval': 150,
+            'html5Support':     true,
+            'before':           function (urlFragment, target) {},
+            'success':          function (urlFragment, target) {},
+            'after':            function (urlFragment, target) {},
+            'error':            function (jqXHR, textStatus, errorThrown, urlFragment, target) {},
+            'insert':           function (urlFragment, target, data) {
+			    $(target).html(data);
+		    },
+            'historyNext':      function(urlFragment, target, data) {
+                this.opts.insert(urlFragment, target, data);
+            },
+            'historyPrev':      function(urlFragment, target, data) {
+                this.opts.insert(urlFragment, target, data);
+            }
+        };
+
+        this.opts = $.extend(this.default, opts);
+
+        this.$el.data(name, this);
+
+        this.$selector = $(this.opts.selector);
+
+        this.init();
+    }
+
+    Asfar.prototype.pushState = function (urlFragment, first) {
+        if (this.opts.html5Support) {
+            history.pushState({type: 'ajax', stamp: (Math.random(0, 9999) * 100000) + (new Date().getTime())}, "title " + this.pagecount,urlFragment);
+            this.pagecount++;
+        }else{
+            location.hash = '#!' + urlFragment;
+            this.currentHash = location.hash;
+        }
+    }
+
+    Asfar.prototype.call = function(urlFragment, target, isHistory) {
+        var self = this;
+
+        self.opts.before(urlFragment, target);
+
+        $.ajax({
+            cache: false,
+            async: true,
+            type: 'GET',
+            dataType: "html",
+            url: urlFragment,
+            success: function(data) {
+                // we need to wrap it, otherwise we can't select it if body is the parent
+                var responseHtml = '<section id="asfar-fake">' + data + '</section>';
+
+                if (0 !== $(responseHtml).find(target).parent().size()) {
+                    data = $(responseHtml).find(target).html();
+                }
+
+                self.opts.insert(urlFragment, target, data);
+
+                if (true !== isHistory) {
+                    self.pushState(urlFragment);
+                }
+
+                self.opts.success(urlFragment, target);
+
+                $(target).trigger('asfar', [urlFragment]);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                self.opts.error(jqXHR, textStatus, errorThrown, urlFragment, target);
+            }
+        });
+
+        // is that really usefull?
+        self.opts.after(urlFragment, target);
+    }
+
+    /**
+     * @todo check viability of delegation
+     */
+    Asfar.prototype.init = function() {
+        var self = this;
+
+        // no matter if you want html5 support or not, if it ain't there it ain't gonna happen
+        if (typeof history.pushState === 'undefined') {
+            this.opts.html5Support = false;
+        }
+
+        // no html5 , no she bang fallback, no asfar *sadface*
+        if (!this.opts.html5Support && !this.opts.sheBangFallback) {
+            return false;
+        }
+
+        if (!this.opts.html5Support) {
+            setInterval(function () {
+                if ('' === location.hash) {
+                    // start
+                    if (null !== self.currentHash){
+                        self.call(location.hash.substring(2), self.opts.target, true);
+                        self.currentHash = null;
+                    }
+                } else {
+                    if (location.hash !== self.currentHash) {
+                        self.call(location.hash.substring(2), self.opts.target, false);
+                    }
+
+                }
+            }, self.opts.sheBangInterval);
+        } else {
+            window.onpopstate = function (event) {
+
+
+
+                if (event.state !== null) {
+                    if ("ajax" === event.state.type) {
+                        console.log(event.state);
+
+                        var i, direction = 'next';
+
+                        console.log(self.historyBackward.length);
+
+
+                        if (0 < self.historyBackward.length) {
+                            self.historyBackward.foreach( function( k, v ) {
+                                console.log(k);
+                                if (k === event.state.stamp) {
+                                    self.historyBackward.splice(i, 9999999);
+                                    direction = 'prev';
+                                }
+                                i++;
+                            });
+                        }
+
+                        console.log(direction);
+
+                        if ('next' === direction) {
+                            self.historyBackward[event.state.stamp] = 'x';
+                            console.log('2 ' + self.historyBackward.length);
+                        }
+
+
+                        self.call(document.location.pathname, self.opts.target, true);
+
+                    }
+                }
+            };
+        }
+
+        // this can be copied in to the header of the html document to increase speed
+        if (!this.opts.html5Support && '#!' === location.hash.substring(0,2)) {
 			location.href = location.hash.substring(2);
 		}
-		
-		if( (typeof(arguments[0]) === 'undefined') || (typeof(arguments[0]['selector']) === 'undefined')){
-			var selector = 'a[href*="' + document.location.host + '"], a[href^="/"], a:not([href^="http://"])';
-		}else{
-			var selector = arguments[0]['selector'];
-		}
-						
-		var target = $(this).selector; //this;
-	
-		var html5AjaxHistoryO = new html5AjaxHistoryC(target,selector);
 
-		if ((typeof(arguments[0]) !== 'undefined')) {
-			if((typeof(arguments[0]['before']) !== 'undefined')){
-				html5AjaxHistoryO.before = arguments[0]['before'];
-			}
-			if((typeof(arguments[0]['insert']) !== 'undefined')){
-				html5AjaxHistoryO.insert = arguments[0]['insert'];
-			}
-			if((typeof(arguments[0]['success']) !== 'undefined')){
-				html5AjaxHistoryO.success = arguments[0]['success'];
-			}
-			if((typeof(arguments[0]['after']) !== 'undefined')){
-				html5AjaxHistoryO.after = arguments[0]['after'];
-			}
-			if((typeof(arguments[0]['error']) !== 'undefined')){
-				html5AjaxHistoryO.error = arguments[0]['error'];
-			}
-		}
-	
-	
-		
-		$(selector).click(function (event) {
-			event.preventDefault();
-			html5AjaxHistoryO.call($(this).attr('href'),target);
-		});
-	}
-})( jQuery );
+        this.$selector.on('click.' + name, function(e) {
+            e.preventDefault();
+
+            self.call($(this).attr('href'), self.opts.target, false);
+        });
+
+        if (this.opts.html5Support) {
+            self.pushState(document.location.pathname);
+        }
+    };
+
+    Asfar.prototype.destroy = function() {
+        this.$selector.off('.' + name);
+
+        this.$el.removeData(name);
+        this.$el = null;
+    };
+
+    $.fn.asfar = function(opts) {
+        var target = $(this).selector;
+
+        // you can not initiate more then one asfar instance at a time because you would need a unique selector (a elements for each asfar content area)
+        if (1 === $(this).size()) {
+            return new Asfar(this, opts, target);
+        }
+    };
+
+})(jQuery, document, window);
